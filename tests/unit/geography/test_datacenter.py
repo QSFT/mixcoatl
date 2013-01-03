b664909ec19760309ea87136b8d33a98225cc734
John E. Vincent <lusis.org+github.com@gmail.com>
2013-01-03 10:37:10 -0500

    more tests

---
+++ tests/unit/geography/test_datacenter.py
@@ -0,0 +1,33 @@
+import os
+# These have to be set before importing any mixcoatl modules
+os.environ['ES_ACCESS_KEY'] = 'abcdefg'
+os.environ['ES_SECRET_KEY'] = 'gfedcba'
+
+import unittest
+from mock import patch
+from mock import Mock
+import mixcoatl.geography.datacenter as datacenter
+import tests.data.datacenter as dc_data
+
+@patch('mixcoatl.resource.Resource.get')
+class TestDataCenters(unittest.TestCase):
+
+    def setUp(self):
+        self.last_error = '{"error":{"message":"job terminated unexpectedly"}}'
+
+    def test_has_all_dcs_and_is_DataCenter(self, mock_data):
+        '''test all() returns a list of DataCenter'''
+        mock_data.return_value = dc_data.all_datacenters
+
+        d = datacenter.DataCenter.all(19344)
+        assert len(d) == 3
+        for x in d:
+            assert isinstance(x, datacenter.DataCenter), '%s must be an instance of DataCenter' % x
+
+    def test_has_a_datacenter(self, mock_data):
+        mock_data.return_value = dc_data.one_datacenter
+
+        d = datacenter.DataCenter(64351)
+        assert d.datacenter_id == 64351
+        assert d.status == 'ACTIVE'
+        assert d.provider_id == 'us-west-2a'
---
+++ tests/unit/geography/test_region.py
@@ -0,0 +1,38 @@
+import os
+# These have to be set before importing any mixcoatl modules
+os.environ['ES_ACCESS_KEY'] = 'abcdefg'
+os.environ['ES_SECRET_KEY'] = 'gfedcba'
+
+import unittest
+from mock import patch
+from mock import Mock
+import mixcoatl.geography.region as region
+import tests.data.region as region_data
+
+@patch('mixcoatl.resource.Resource.get')
+class TestRegions(unittest.TestCase):
+
+    def setUp(self):
+        self.last_error = '{"error":{"message":"job terminated unexpectedly"}}'
+
+    def test_has_all_regions_and_is_Region(self, mock_data):
+        '''test all() returns a list of Region'''
+        mock_data.return_value = region_data.all_regions
+
+        d = region.Region.all()
+        assert len(d) == 8
+        for x in d:
+            assert isinstance(x, region.Region), '%s must be an instance of Region' % x
+
+    def test_has_a_region(self, mock_data):
+        mock_data.return_value = region_data.one_region
+
+        d = region.Region(19341)
+        # test primary_key
+        assert d.region_id == 19341
+        # test lazy property
+        assert d.status == 'ACTIVE'
+        # test uncameled property
+        assert d.provider_id == 'ap-northeast-1'
+        # test nested uncameled key
+        assert d.customer['customer_id'] == 14334
