563a4ec95dc92a42d1f39d6551c39c4abfcb1206
John E. Vincent <lusis.org+github.com@gmail.com>
2013-01-03 02:33:43 -0500

    finishing out job tests

--- tests/admin/test_job.py
+++ tests/admin/test_job.py
@@ -5,11 +5,16 @@ os.environ['ES_SECRET_KEY'] = 'gfedcba'
 
 import unittest
 from mock import patch
+from mock import Mock
+from mixcoatl.resource import Resource
 import mixcoatl.admin.job as job
 import tests.data.job as job_data
 
 @patch('mixcoatl.resource.Resource.get')
 class TestJobs(unittest.TestCase):
+
+    def setUp(self):
+        self.last_error = '{"error":{"message":"job terminated unexpectedly"}}'
 
     def test_all_jobs(self,mock_job_get):
         '''test Job.all()'''
@@ -31,5 +36,18 @@ class TestJobs(unittest.TestCase):
         assert j['status'] == 'COMPLETE'
 
 
+    def test_job_get_last_error(self,mock_job_get):
+        '''test failure on job.get()'''
+        with patch('mixcoatl.resource.Resource.last_error', autospec=True) as mock_last_error:
+            mock_last_error.__get__ = Mock(return_value=self.last_error)
+            j = job.get(12345)
+            assert j == self.last_error
+
+    def test_all_last_error(self,mock_job_get):
+        '''test failure on all()'''
+        with patch('mixcoatl.resource.Resource.last_error', autospec=True) as mock_last_error:
+            mock_last_error.__get__ = Mock(return_value=self.last_error)
+            j = job.Job.all()
+            assert j == self.last_error
 
 
