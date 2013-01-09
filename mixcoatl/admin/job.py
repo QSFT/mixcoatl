18db53277bc36629cd7abc5f11b0ba057ba3920b
John E. Vincent <lusis.org+github.com@gmail.com>
2013-01-09 14:14:13 -0500

    testing documentation options

--- mixcoatl/admin/job.py
+++ mixcoatl/admin/job.py
@@ -10,41 +10,59 @@ class Job(Resource):
     primary_key = 'job_id'
 
     def __init__(self, job_id = None, **kwargs):
+        """
+        Initialize a new Job object from the provided job_id
+        """
         Resource.__init__(self)
         self.__job_id = job_id
 
     @property
     def job_id(self):
+        """
+        Return the job's ID. Immutable
+        """
         return self.__job_id
 
     @lazy_property
     def status(self):
+        """
+        Return the job's status. Immutable
+        """
         return self.__status
 
     @lazy_property
     def description(self):
+        """
+        Return the job's description. Immutable
+        """
         return self.__description
 
     @lazy_property
     def message(self):
+        """
+        Return the job's message. Immutable
+        """
         return self.__message
 
     @lazy_property
     def start_date(self):
+        """
+        Return the job's start date. Immutable
+        """
         return self.__start_date
 
     @lazy_property
     def end_date(self):
+        """
+        Return the job's stop date. Immutable
+        """
         return self.__end_date
-
-
-    def get_job(self, job_id):
-        p = self.path+"/"+str(job_id)
-        return self.get(path=p)
-
 
     @classmethod
     def all(cls):
+        """Get all jobs
+        :returns: a list of ``Job`` objects
+        """
         r = Resource(cls.path)
         x = r.get()
         if r.last_error is None:
@@ -54,6 +72,14 @@ class Job(Resource):
 
     @classmethod
     def wait_for(cls, job_id, status='COMPLETE', callback = None):
+        """Blocks execution until ``job_id`` returns ``status``
+        :param job_id: The ID of the job to wait on
+        :type job_id: int.
+        :param status: The status to expect before continuing
+        :type status: str.
+        :param callback: Optional callback to be passed the final ``Job`` when ``status`` is reached
+        :type callback: func.
+        """
         j = Job(job_id)
         initial_status = j.status
         if initial_status == 'ERROR':
