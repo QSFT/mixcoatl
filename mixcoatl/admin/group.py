71606b0b113990a5f61e8d5682b96256c1242ca0
John E. Vincent <lusis.org+github.com@gmail.com>
2013-01-11 16:34:18 -0500

    adding docs to group and role

--- mixcoatl/admin/group.py
+++ mixcoatl/admin/group.py
@@ -12,34 +12,64 @@ class Group(Resource):
 
     @property
     def group_id(self):
+        """`int` - The unique id of this group in enStratus"""
         return self.__group_id
 
     @lazy_property
     def description(self):
+        """`str` - The user-friendly description of this group"""
         return self.__description
 
     @lazy_property
     def name(self):
+        """`str` - The name of the group"""
         return self.__name
 
     @lazy_property
     def status(self):
+        """`str` - The current status of the group in enStratus"""
         return self.__status
 
     @lazy_property
     def customer(self):
+        """`dict` - The customer to who this group belongs."""
         return self.__customer
 
     @classmethod
-    def all(cls, **kwargs):
+    def all(cls, keys_only=False, **kwargs):
+        """Get all groups
+
+        .. note::
+
+            The keys used to make the request determine results visibility
+
+        :param keys_only: Only return `group_id` instead of `Group` objects
+        :type keys_only: bool.
+        :param detail: The level of detail to return - `basic` or `extended`
+        :type detail: bool.
+        :param account_id: Restrict results to `account_id`
+        :type account_id: int.
+        :returns: `list` - List of :class:`Group` or :attr:`group_id`
+        :raises: :class:`GroupException`
+        """
         r = Resource(cls.path)
         if 'details' in kwargs:
             r.request_details = kwargs['details']
         else:
             r.request_details = 'basic'
 
-        x = r.get()
-        if r.last_error is None:
-            return [cls(i['groupId']) for i in x[cls.collection_name]]
+        if 'account_id' in kwargs:
+            params = {'accountId': kwargs['account_id']}
         else:
-            return x.last_error
+            params = {}
+
+        x = r.get(params=params)
+        if r.last_error is None:
+            if keys_only is True:
+                return [i['groupId'] for i in x[cls.collection_name]]
+            else:
+                return [cls(i['groupId']) for i in x[cls.collection_name]]
+        else:
+            raise GroupException(r.last_error['error']['message'])
+
+class GroupException(BaseException): pass
--- mixcoatl/admin/role.py
+++ mixcoatl/admin/role.py
@@ -2,6 +2,8 @@ from mixcoatl.resource import Resource
 from mixcoatl.decorators.lazy import lazy_property
 
 class Role(Resource):
+    """A role defines a common set of permissions that govern access into a given account"""
+
     path = 'admin/Role'
     collection_name = 'roles'
     primary_key = 'role_id'
@@ -12,38 +14,72 @@ class Role(Resource):
 
     @property
     def role_id(self):
+        """`int` - The unique id of the role"""
         return self.__role_id
 
     @lazy_property
     def acl(self):
+        """`dict` - The access permissions associated with the role"""
         return self.__acl
 
     @lazy_property
     def description(self):
+        """`str` - A user-friendly description of the role"""
         return self.__description
 
     @lazy_property
     def customer(self):
+        """`dict` - The customer to whom this role belongs"""
         return self.__customer
 
     @lazy_property
     def name(self):
+        """`str` - The name of the role"""
         return self.__name
 
     @lazy_property
     def status(self):
+        """`str` - The status of the role in enStratus"""
         return self.__status
 
     @classmethod
-    def all(cls, **kwargs):
+    def all(cls, keys_only = False, **kwargs):
+        """Get all roles
+
+        .. note::
+
+            The keys used to make the request determine results visibility
+
+        :param keys_only: Only return :attr:`role_id` instead of :class:`Group` objects
+        :type keys_only: bool.
+        :param detail: The level of detail to return - `basic` or `extended`
+        :type detail: str.
+        :param account_id: List roles with mappings to groups in the specified account
+        :type account_id: int.
+        :param group_id: Provides teh role associated with the specified group
+        :type group_id: int.
+        :returns: :class:`RoleException`
+        """
         r = Resource(cls.path)
+        params = {}
         if 'details' in kwargs:
             r.request_details = kwargs['details']
         else:
             r.request_details = 'basic'
 
-        x = r.get()
+        if 'account_id' in kwargs:
+            params['account_id'] = kwargs['account_id']
+
+        if 'group_id' in kwargs:
+            params['group_id'] = kwargs['group_id']
+
+        x = r.get(params=params)
         if r.last_error is None:
-            return [cls(i['roleId']) for i in x[cls.collection_name]]
+            if keys_only is True:
+                return [i['roleId'] for i in x[cls.collection_name]]
+            else:
+                return [cls(i['roleId']) for i in x[cls.collection_name]]
         else:
-            return x.last_error
+            raise RoleException(x.last_error['error']['message'])
+
+class RoleException(BaseException): pass
