3ddc522479a1dcee44d1125a65836180eebd5a99
John E. Vincent <lusis.org+github.com@gmail.com>
2013-01-30 09:10:55 -0500

    adding firewall changes

--- mixcoatl/network/firewall.py
+++ mixcoatl/network/firewall.py
@@ -1,5 +1,8 @@
+"""Implements the enStratus Firewall API"""
 from mixcoatl.resource import Resource
 from mixcoatl.decorators.lazy import lazy_property
+from mixcoatl.network.firewall_rule import FirewallRule
+from mixcoatl.utils import camelize, camel_keys
 
 class Firewall(Resource):
     PATH = 'network/Firewall'
@@ -8,66 +11,83 @@ class Firewall(Resource):
 
     def __init__(self, firewall_id = None, *args, **kwargs):
         Resource.__init__(self)
+
+        if 'detail' in kwargs:
+            self.request_details = kwargs['detail']
+
         self.__firewall_id = firewall_id
 
     @property
     def firewall_id(self):
+        """`str` - The unique enStratus ID for this firewall"""
         return self.__firewall_id
 
     @lazy_property
     def status(self):
+        """`str` - The status of this firewall"""
         return self.__status
 
     @lazy_property
     def label(self):
+        """`str` - A color label assigned to this firewall"""
         return self.__label
 
     @lazy_property
     def owning_account(self):
+        """`dict` - The enStratus account under which this firewall is registered"""
         return self.__owning_account
 
     @lazy_property
     def name(self):
+        """`str` - The user-friendly name for this firewall"""
         return self.__name
 
     @lazy_property
     def owning_user(self):
+        """`dict` or `None` - The enStratus owner of record for this firewall"""
         return self.__owning_user
 
     @lazy_property
     def description(self):
+        """`str` - The description of this firewall in enStratus"""
         return self.__description
 
     @lazy_property
     def cloud(self):
+        """`dict` - The enStratus cloud account in which this firewall lives"""
         return self.__cloud
 
     @lazy_property
     def provider_id(self):
+        """`str` - The cloud provider unique id for this firewall"""
         return self.__provider_id
 
     @lazy_property
     def region(self):
+        """`dict` - The region in which this firewall operates"""
         return self.__region
 
     @lazy_property
     def removable(self):
+        """`bool` - Indicates if this firewall can be removed"""
         return self.__removable
 
     @lazy_property
     def budget(self):
+        """`int` - The enStratus billing code costs are associated with"""
         return self.__budget
 
     @lazy_property
     def customer(self):
+        """`dict` - The enStratus customer to which this firewall belongs"""
         return self.__customer
 
     @property
     def rules(self):
+        """`list` - The firewall rules associated with this firewall"""
         try:
             return self.__rules
         except AttributeError:
-            from mixcoatl.network.firewall_rule import FirewallRule
             rls = FirewallRule.all(self.__firewall_id)
             if len(rls) < 1:
                 self.__rules = []
@@ -76,14 +96,51 @@ class Firewall(Resource):
             return rls
 
     @classmethod
-    def all(cls, region_id):
-        from mixcoatl.utils import uncamel_keys
+    def all(cls, region_id, **kwargs):
+        """List all firewalls in `region_id`
+
+        :param region_id: The region to list firewalls for
+        :type region_id: int.
+        :param detail: Level of detail to return - `basic` or `extended`
+        :type detail: str.
+        :param keys_only: Return only :attr:`firewall_id` in results
+        :type keys_only: bool.
+        :returns: `list` of :attr:`firewall_id` or :class:`Firewall`
+        :raises: :class:`FirewallException`
+        """
+
+        params = {}
         r = Resource(cls.PATH)
-        r.request_details = 'extended'
-        params = {'regionId':region_id}
-        c = r.get(params=params)
-        if r.last_error is None:
-            return [cls(i['firewallId']) for i in c[cls.COLLECTION_NAME]]
-            #return uncamel_keys(c)
+        r.request_details = 'none'
+
+        if 'detail' in kwargs:
+            request_details = kwargs['detail']
         else:
-            return r.last_error
+            request_details = 'extended'
+
+        if 'keys_only' in kwargs:
+            keys_only = kwargs['keys_only']
+        else:
+            keys_only = False
+
+        params['regionId'] = region_id
+        x = r.get(params=params)
+        if r.last_error is None:
+            keys = [cls(i[camelize(cls.PRIMARY_KEY)]) for i in x[cls.COLLECTION_NAME]]
+            if keys_only is True:
+                firewalls = keys
+            else:
+                firewalls = []
+                for i in x[cls.COLLECTION_NAME]:
+                    key = i[camelize(cls.PRIMARY_KEY)]
+                    fw = cls(key)
+                    fw.request_details = request_details
+                    fw.load()
+                    firewalls.append(fw)
+            return firewalls
+        else:
+            raise FirewallException(r.last_error)
+
+class FirewallException(BaseException):
+    """Generic Firewall Exception"""
+    pass
--- mixcoatl/network/firewall_rule.py
+++ mixcoatl/network/firewall_rule.py
@@ -1,5 +1,11 @@
 from mixcoatl.resource import Resource
 from mixcoatl.decorators.lazy import lazy_property
+from mixcoatl.decorators.validations import required_attrs
+from mixcoatl.admin.job import Job
+from mixcoatl.utils import camel_keys
+from mixcoatl.utils import camelize
+
+import json
 
 class FirewallRule(Resource):
     PATH = 'network/FirewallRule'
@@ -7,38 +13,203 @@ class FirewallRule(Resource):
     PRIMARY_KEY = 'firewall_rule_id'
 
     def __init__(self, firewall_rule_id = None, *args, **kwargs):
+        if 'detail' in kwargs:
+            self.request_details = kwargs['detail']
+
         Resource.__init__(self)
         self.__firewall_rule_id = firewall_rule_id
 
     @property
     def firewall_rule_id(self):
+        """`int` - The unique enStratus id for this rule"""
         return self.__firewall_rule_id
 
     @lazy_property
     def direction(self):
+        """`str` - The direction of the rule: `ingress` or `egress`"""
         return self.__direction
+
+    @direction.setter
+    def direction(self, d):
+        self.__direction = d
 
     @lazy_property
     def firewall(self):
+        """`dict` - The firewall to which this rule belongs"""
         return self.__firewall
+
+    @firewall.setter
+    def firewall(self, fwid):
+        self.__firewall = {'firewall_id': fwid}
 
     @lazy_property
     def network_address(self):
+        """`str` - The CIDR for the source or destination of traffic"""
         return self.__network_address
+
+    @network_address.setter
+    def network_address(self, addr):
+        self.__network_address = addr
 
     @lazy_property
     def protocol(self):
+        """`str` - The network protocol for this rule"""
         return self.__protocol
 
-    @classmethod
-    def all(cls, firewall_id):
-        from mixcoatl.utils import uncamel_keys
-        r = Resource(cls.PATH)
-        r.request_details = 'extended'
-        params = {'firewallId':firewall_id}
-        c = r.get(params=params)
-        if r.last_error is None:
-            rules = [i['firewallRuleId'] for i in c[cls.COLLECTION_NAME]]
-            return [cls(rule) for rule in rules]
+    @protocol.setter
+    def protocol(self, p):
+        self.__protocol = p
+
+    @lazy_property
+    def start_port(self):
+        """`int` or `None` - The start port for this rule"""
+        return self.__start_port
+
+    @start_port.setter
+    def start_port(self, p):
+        self.__start_port = p
+
+    @lazy_property
+    def end_port(self):
+        """`int` or `None` - The end port for this rule"""
+        return self.__end_port
+
+    @end_port.setter
+    def end_port(self, p):
+        self.__end_port = p
+
+    @required_attrs(['firewall', 'network_address', 'protocol', 'direction', 'start_port', 'end_port'])
+    def create(self, **kwargs):
+        """Create a new firewall rule
+
+            .. warning::
+
+                Does not currently support adding ICMP rules
+
+        :param reason: Reason for the new rule
+        :type reason: str.
+        :returns: `bool`
+        :raises: :class:`FirewallRuleException`
+        """
+
+        if 'reason' not in kwargs:
+            reason = 'Added by mixcoatl'
         else:
-            return r.last_error
+            reason = kwargs['reason']
+
+        payload = {'add_rule':[{
+            'firewall_id': self.firewall['firewall_id'],
+            'direction': self.direction,
+            'start_port': self.start_port,
+            'end_port': self.end_port,
+            'protocol': self.protocol,
+            'reason': reason,
+            'cidr' : self.network_address}]}
+
+        if self.firewall_rule_id is not None:
+            raise FirewallRuleException('Cannot modify existing firewall rule')
+
+        self.post(self.PATH, data=json.dumps(camel_keys(payload)))
+
+        if self.last_error is None:
+            return True
+        else:
+            if self.last_request.status_code == 418:
+                return True
+            else:
+                raise FirewallRuleException(self.last_error)
+
+    @required_attrs(['firewall_rule_id'])
+    def remove(self, reason):
+        """Remove a firewall rule
+
+        :param reason: Reason for removing the rule
+        :type reason: str.
+        :returns: `bool`
+        :raises: :class:`FirewallRuleException`
+        """
+
+        params = {'reason': reason}
+        self.delete(self.PATH+'/'+str(self.firewall_rule_id), params=params)
+
+        if self.last_error is None:
+            return True
+        else:
+            raise FirewallRuleException(self.last_error)
+
+    @classmethod
+    def all(cls, firewall_id, **kwargs):
+        """List all rules for `firewall_id`
+
+        :param firewall_id: The id of the firewall to list rules for
+        :type firewall_id: int.
+        :param detail: Level of detail to return - `basic` or `extended`
+        :type detail: str.
+        :param keys_only: Return only :attr:`firewall_rule_id` in results
+        :type keys_only: bool.
+        :returns: `list` of :attr:`firewall_rule_id` or :class:`FirewallRule`
+        :raises: :class:`FirewallRuleException`
+        """
+
+        params = {}
+        r = Resource(cls.PATH)
+        r.request_details = 'none'
+        if 'detail' in kwargs:
+            request_details = kwargs['detail']
+        else:
+            request_details = 'extended'
+
+        if 'keys_only' in kwargs:
+            keys_only = kwargs['keys_only']
+        else:
+            keys_only = False
+
+        params['firewallId'] = firewall_id
+        x = r.get(params=params)
+        if r.last_error is None:
+            keys = [i[camelize(cls.PRIMARY_KEY)] for i in x[cls.COLLECTION_NAME]]
+            if keys_only is True:
+                rules = keys
+            else:
+                rules = []
+                for i in x[cls.COLLECTION_NAME]:
+                    key = i[camelize(cls.PRIMARY_KEY)]
+                    rule = cls(key)
+                    rule.request_details = request_details
+                    rule.load()
+                    rules.append(rule)
+            return rules
+        else:
+            raise FirewallRuleException(r.last_error)
+
+def add_rule(firewall_id, network, proto, direction, start, end, reason):
+    """Add a firewall rule to a firewall
+
+    >>> f = add_rule(136663, '10.1.1.1/32', 'TCP', 'INGRESS', 15000, 15000, 'inbound api')
+
+    """
+    f = FirewallRule()
+    f.firewall = firewall_id
+    f.network_address = network
+    f.protocol = proto
+    f.direction = direction
+    f.start_port = start
+    f.end_port = end
+    return f.create(reason=reason)
+
+def delete_rule(rule_id, reason='rule removed by mixcoatl'):
+    """Remove a firewall rule
+
+    :param rule_id: The id of the firewall rule to remove
+    :type rule_id: int.
+    :param reason: The reason for removing the rule
+    :type reason: string
+    :returns: `bool`
+    :raises: :class:`FirewallRuleException`
+    """
+    f = FirewallRule(rule_id)
+    return f.remove(reason)
+
+class FirewallRuleException(BaseException):
+    """Generic Exception for FirewallRules"""
+    pass
