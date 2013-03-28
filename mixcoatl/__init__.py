488e8a9a3db559c81f9cf8e070d26355e589c4af
Greg Moselle <greg.moselle@enstratus.com>
2013-03-28 09:02:15 -0700

    fixed small typo s/authord/author

--- mixcoatl/__init__.py
+++ mixcoatl/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'mixcoatl'
 __version__ = '0.2.6'
-__authord__ = 'John E. Vincent'
+__author__ = 'John E. Vincent'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2012-2013 John E. Vincent, enStratus Networks Inc'
