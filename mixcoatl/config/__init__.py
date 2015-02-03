3f8e2315ea6e49f667f9db2f9786bb95d378d000
Brian Williams <brian@mxapps.com>
2015-02-03 09:28:40 -0600

    Can now specify config(s) instead of env variables.  Resolves #134

--- mixcoatl/config/__init__.py
+++ mixcoatl/config/__init__.py
@@ -1,15 +1,9 @@
 import os
+import glob
+import shutil
 import datetime
-
 from mixcoatl.exceptions import ConfigException
-
-
-def validate(date_text):
-    try:
-        datetime.datetime.strptime(date_text, '%Y-%m-%d')
-        return date_text
-    except ValueError:
-        raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+from subprocess import call
 
 
 class Config(object):
@@ -24,8 +18,40 @@ class Config(object):
         self.basepath = None
         self.default_api_version = '2014-07-30'
         self.ssl_verify = None
+        self.mixcoatl_dir = os.path.expanduser('~')+"/.mixcoatl"
 
     def configure(self):
+        if not os.path.exists(self.mixcoatl_dir):
+            os.makedirs(self.mixcoatl_dir)
+
+        if 'ES_ACCESS_KEY' not in os.environ and 'DCM_ACCESS_KEY' not in os.environ:
+            if not os.path.exists(self.mixcoatl_dir+"/default"):
+                try:
+                    # Python 2
+                    prompt = raw_input
+                except NameError:
+                    # Python 3
+                    prompt = input
+
+                cfg_num = 0
+                file_list = glob.glob(self.mixcoatl_dir+"/*.config")
+                for i in file_list:
+                    print ">>> Type" , cfg_num , "for" , i.replace(self.mixcoatl_dir+"/", "").replace(".config", "")
+                    cfg_num += 1
+
+                cfg_pick = prompt("Which config would you like to use? ")
+
+                with open(file_list[int(cfg_pick)]) as f:
+                    for line in f:
+                        k, v = line.split('=', 1)
+                        os.environ[k] = v.strip()
+                shutil.copy(file_list[int(cfg_pick)], self.mixcoatl_dir+"/default")
+            else:
+                with open(self.mixcoatl_dir+"/default") as f:
+                    for line in f:
+                        k, v = line.split('=', 1)
+                        os.environ[k] = v.strip()
+
         if self.access_key is None:
             if 'ES_ACCESS_KEY' in os.environ and 'DCM_ACCESS_KEY' not in os.environ:
                 os.environ['DCM_ACCESS_KEY'] = os.environ['ES_ACCESS_KEY']
@@ -54,7 +80,7 @@ class Config(object):
                 if 'DCM_ENDPOINT' in os.environ:
                     str = os.environ['DCM_ENDPOINT'].split('/')
 
-                    if validate(str[-1]):
+                    if self.validate(str[-1]):
                         self.set_api_version(str[-1])
                     else:
                         self.set_api_version(self.default_api_version)
@@ -80,6 +106,13 @@ class Config(object):
                 self.set_ssl_verify(os.environ['DCM_SSL_VERIFY'])
             else:
                 self.set_ssl_verify('1')
+
+    def validate(self, date_text):
+        try:
+            datetime.datetime.strptime(date_text, '%Y-%m-%d')
+            return date_text
+        except ValueError:
+            raise ValueError("Incorrect data format, should be YYYY-MM-DD")
 
     def set_access_key(self, key):
         self.access_key = key
