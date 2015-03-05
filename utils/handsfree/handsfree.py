1185a2445a519b3ff79cc12f4868fccb70ba3ac6
zomgreg <greg.moselle@enstratius.com>
2015-03-04 22:17:16 -0600

    handsfree progress

---
+++ utils/handsfree/handsfree.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+
+import os
+
+import json
+import string
+import random
+from subprocess import call
+from termcolor import colored
+from fabric.api import cd, path, hide, sudo, run, execute, settings, get, put
+from fabric.contrib.files import exists
+
+
+class FabricSupport:
+    MASTER_API_KEY = None
+    MASTER_SECRET_KEY = None
+
+    def __init__(self, hosts, version, console_host, license_key, download_pass, release_path, email, first_name, last_name, company_name):
+        self.hosts = hosts
+        self.version = version
+        self.console_host = console_host
+        self.license_key = license_key
+        self.download_pass = download_pass
+        self.release_path = release_path
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.company_name = company_name
+        self.sbin_dir = '/services/backend/sbin'
+        self.cloud_descriptors_dir = './clouds/descriptors'
+        self.cloud_credentials_dir = './clouds/credentials'
+        pass
+
+    def random_pass(self):
+        rnd = random.SystemRandom()
+        return ''.join(rnd.choice(string.ascii_letters + string.digits) for i in range(11))
+
+    def fetch_vm_info(self):
+        output = run("cat /etc/issue")
+        parse_string = output.split()
+        print "*** OS Detected:  " + parse_string[0] + " " + parse_string[1]
+
+    def deps(self):
+        print "{:80}".format("Installing dependencies "),
+        sudo('apt-get install unzip -y')
+        print "{:}".format('[ ' + colored('OK', 'green') + ' ]')
+
+    def fetch_release(self):
+        if exists(self.release_path, use_sudo=True):
+            print "{:80} {:}".format("DCM Directory (" + self.release_path + ") detected. ", '[ ' + colored('SKIPPING FETCH', 'yellow') + ' ]')
+        else:
+            print "{:80}".format("Fetching release " + self.version),
+            # To do: fix this redundancy
+            with cd("/tmp"):
+                run("curl -o /tmp/es-onpremise-chef-solo-{}.tar.gz https://onpremise:{}@download.ext.enstratius.com/{}/es-onpremise-chef-solo-{}.tar.gz".format(self.version,self.download_pass,self.version,self.version))
+                print "{:}".format('[ ' + colored('OK', 'green') + ' ]')
+
+                print "{:80}".format("Extracting release to " + self.release_path),
+                run("tar -zxf es-onpremise-chef-solo-" + self.version + ".tar.gz -C /tmp")
+                print "{:}".format('[ ' + colored('OK', 'green') + ' ]')
+
+    def run_setup(self):
+        setup_file = self.release_path + \
+            "/local_settings/handsfree/single_node.json"
+        if exists(setup_file, use_sudo=True):
+            print "{:80} {:}".format("Setup File detected. ", '[ ' + colored('SKIPPING SETUP', 'yellow') + ' ]')
+        else:
+            print "{:80}".format("Running DCM setup"),
+            with cd(self.release_path):
+                sudo("./setup.sh -s handsfree -l " +
+                     self.license_key + " -p " + self.download_pass + " -c " + self.console_host)
+            print "{:}".format('[ ' + colored('OK', 'green') + ' ]')
+
+    def install_release(self):
+        flags_file = "/tmp/feature-flags-rules.json"
+        if exists(flags_file, use_sudo=True):
+            print "{:80} {:}".format("Installation already completed. ", '[ ' + colored('SKIPPING DCM INSTALL', 'yellow') + ' ]')
+        else:
+            print "{:80}".format("Running DCM install"),
+            with path('/opt/dmcm-base/bin:/opt/dmcm-base/embedded/bin:/opt/dmcm-base/bin:/opt/dmcm-base/embedded/bin'):
+                with cd(self.release_path):
+                    sudo(
+                        "chef-solo -j local_settings/handsfree/single_node.json -c solo.rb -o 'role[single_node]'")
+            print "{:}".format('[ ' + colored('OK', 'green') + ' ]')
+
+    def create_master_api_key(self):
+        master_apikey = "/tmp/master-apikey.json"
+        if exists(master_apikey, use_sudo=True):
+            print "{:80} {:}".format("Master Key file exists. ", '[ ' + colored('SKIPPING KEYGEN', 'yellow') + ' ]')
+        else:
+            print "{:80}".format("Generating Master Keys"),
+            sudo('/services/backend/sbin/create-master-apikey.sh -o /tmp/master-apikey.json')
+            print "{:}".format('[ ' + colored('OK', 'green') + ' ]')
+
+    def create_initial_user(self):
+
+        '''
+        This method creates the initial user account in DCM.
+
+        It completes the "registration" step for a newly installed environment.
+        '''
+
+        with open('users/initial-account.json', 'r') as initial_account:
+
+            put(initial_account, '/tmp/initial-account.json')
+
+        cmd = '{}/create-initial-user.py --context /tmp/master-apikey.json /tmp/initial-account.json'.format(self.sbin_dir)
+        sudo(cmd)
+        print "{:}".format('[ ' + colored('OK', 'green') + ' ]')
+
+        # initial_user = "/tmp/initial_user.json"
+        # if exists(initial_user, use_sudo=True):
+        #     print "{:80} {:}".format("Initial User file exists. ", '[ ' + colored('SKIPPING USER CREATION', 'yellow') + ' ]')
+        # else:
+        #     print "{:80}".format("Creating Initial User"),
+        #     with open("users/initial_user.json", "r") as myfile:
+        #         data = json.loads(myfile.read())
+        #
+        #     data['firstName'] = self.first_name
+        #     data['lastName'] = self.last_name
+        #     data['email'] = self.email
+        #     data['businessName'] = self.company_name
+        #     data['password'] = self.random_pass()
+        #     data['windowsPassword'] = self.random_pass()
+        #
+        #     with open(os.path.expanduser('~') + '/.ssh/id_rsa.pub', "r") as sshkey:
+        #         key = sshkey.read().rstrip('\n')
+        #
+        #     data['sshPublicKey'] = key
+        #
+        #     sudo('echo \'' + json.dumps(data) + '\' > /tmp/initial_user.json')
+        #     sudo('/services/backend/sbin/create-initial-user.py --context /tmp/master-apikey.json /tmp/initial_user.json')
+        #     print "{:}".format('[ ' + colored('OK', 'green') + ' ]')
+
+    def create_initial_cloud_accounts(self):
+        '''
+        Adds the initial cloud accounts credential
+        '''
+
+        print "{:80}".format("Creating Initial Cloud Account(s)")
+        put('./clouds/initial.json', '/tmp/initial_cloud.json')
+        cmd = '{}/create-initial-cloudaccount.py {} /tmp/initial_cloud.json --context /tmp/master-apikey.json'.format(self.sbin_dir, self.email)
+        print cmd
+        sudo(cmd)
+
+        print "{:}".format('[ ' + colored('OK', 'green') + ' ]')
+
+    def add_private_clouds(self):
+
+        print "{:80}".format("Adding Private Clouds"),
+        for private_cloud in os.listdir(self.cloud_descriptors_dir):
+
+             put(self.cloud_descriptors_dir+'/'+private_cloud, '/tmp/private-cloud.json')
+             cmd = 'bash {}/add-cloud.sh /tmp/private-cloud.json'.format(self.sbin_dir)
+             print cmd
+
+             sudo(cmd)
+
+        print "{:}".format('[ ' + colored('OK', 'green') + ' ]')
+
+    def create_user_api_key(self):
+        '''
+        Creates a user API key and stores it locally in a file called userkeys.json
+        '''
+
+        print "{:80}".format("Create User API Key")
+
+        cmd='{}/create-initial-apikeys.py ' \
+            '--genUserKey ' \
+            '--initialUser /tmp/initial-account.json ' \
+            '--initialAccount /tmp/initial_cloud.json ' \
+            '--output /tmp/userkeys.json ' \
+            '--context /tmp/master-apikey.json'.format(self.sbin_dir)
+
+        print cmd
+        sudo(cmd)
+
+        get('/tmp/userkeys.json', './userkeys.json')
+
+        print "{:}".format('[ ' + colored('OK', 'green') + ' ]')
+
+
+    def set_user_credentials(self):
+        '''
+        Creates a local config file.
+        '''
+
+        print "{:80}".format("Adding Additional Clouds")
+
+        with open('./userkeys.json','r') as f:
+            contents=json.loads(f.read())
+
+        secret_key=contents['secretKey']
+        access_key=contents['accessKey']
+
+        with open('./config.txt','w') as mixcoatl_config:
+            mixcoatl_config.write("export DCM_ACCESS_KEY={}\n".format(access_key))
+            mixcoatl_config.write("export DCM_SECRET_KEY={}\n".format(secret_key))
+            mixcoatl_config.write("export DCM_ENDPOINT=https://{}:15000/api/enstratus/2015-01-28\n".format(self.hosts))
+
+        os.environ["DCM_ACCESS_KEY"] = access_key
+        os.environ["DCM_SECRET_KEY"] = secret_key
+        os.environ["DCM_ENDPOINT"] = 'https://{}:15000/api/enstratus/2015-01-28'.format(self.hosts)
+        os.environ["DCM_SSL_VERIFY"] = '0'
+
+        for credentials_file in os.listdir(self.cloud_credentials_dir):
+            cmd = "dcm-post admin/Account --json {}".format(self.cloud_credentials_dir+'/'+credentials_file)
+            call(cmd, shell=True)
+
+        print "{:}".format('[ ' + colored('OK', 'green') + ' ]')
+
+    def install_parachute(self):
+
+        install_file = "/opt/parachute/app/cfg/config.ini"
+        if exists(install_file, use_sudo=True):
+            print "{:80} {:}".format("Parachute already installed. ", '[ ' + colored('SKIPPING PARACHUTE INSTALL', 'yellow') + ' ]')
+        else:
+            print "{:80}".format("Installing Parachute"),
+            with cd("/tmp"):
+                sudo(
+                    "curl http://download.parachuteapp.net/install.sh | sudo bash -s - -v k30-rc1")
+            print "{:}".format('[ ' + colored('OK', 'green') + ' ]')
+
+    def execute(self, task):
+        with hide('output', 'running', 'warnings'), settings(warn_only=True):
+            get_task = "task = self.%s" % task
+            exec get_task
+            execute(task, hosts=self.hosts)
