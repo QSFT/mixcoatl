6b351b6ff1b5e3242c26749d93c79e12d6a5f31e
zomgreg <greg.moselle@enstratius.com>
2015-03-10 17:05:48 -0500

    added a dry run function that prints a pretty picture of the setup directory

--- utils/handsfree/pre_run.py
+++ utils/handsfree/pre_run.py
@@ -1,10 +1,19 @@
+import os
 
-def dry_run():
+def dry_run(setup_dir):
     '''
     Lists things that will be done as part of the install.
     :return:
     '''
-    pass
+
+    # Pretty print the directory structure of the setup directory
+    for root, dirs, files in os.walk(setup_dir):
+        level = root.replace(setup_dir, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
 
 def sanity_check():
     '''
